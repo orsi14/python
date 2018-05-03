c2a054492af068840163a83dfc4e01b3ecade4de
Mark O <mark.orsi@gmail.com>
2018-05-03 22:15:49 +0100

    added a bunch of comments.  broke stuff.

--- 2048.py
+++ 2048.py
@@ -9,13 +9,10 @@ def print_matrix():
 			print(matrix[a][b], "\t|", end=' ')
 		print()
 	print
-
+						
+				
 def move(matrix, direction="left"):
-	# TO-DO: transpose function is throwing an error - not sure why - might be namespace related.
-	# this function was working for left and right, but not up and down
-	# for up and down, i added a transpose function so that it could use the same code as left/right
-	# the direction lookup probably needs to be reviewed as it has too many options - it should be simpler by using the transpose to deal with up/down
-	
+
 	# define a bunch of values for each direction of motion
 	directionLookup = {"left": [-1,1,game_size-1,1], "right": [1,game_size-2,0,-1], "up": [-1,1,game_size-1,1], "down": [1,game_size-2,0,-1]}
 	
@@ -32,23 +29,37 @@ def move(matrix, direction="left"):
 	# then move to the next cell and do the same
 	hasMoved = False
 	
+	#go through every row (arow will be 0,1,2,3)
 	for arow in range(game_size):
+		#within each row, go through each column (1,2,3) or (2,1,0), either left to right or right to left
 		for acol in range(startCol,endCol+stepDir,stepDir):
-			if matrix[arow][acol] is not "":
+				
+			# the first thing we're going to do is combine any cells that should be combined (same value as the cell next to it)
+			# constraint is that only 2 cells can be merged together, no double merging or multi-step merging
+			
+			#if the box is not empty - if it's empty we dont care about moving anything in it
+			if matrix[arow][acol] is not " ":
+				# in this block, we've found a number.  
+				# start looking in the previous cell, which has index of checkCol
 				checkCol = acol+dirCol
 				
-				#print "Found a thing to move.",
-				#print "Box of interest at (", a, ",",b, ") is ", matrix[a][b] 
-				#print "To its left has a value of ", matrix[a][checkCol]
+				print("Found a thing to move. It's value is '",matrix[arow][acol],"'",)
+				print("Box of interest at (", arow, ",",acol, ") is ", matrix[arow][acol])
+				print("To its", direction, "has a value of ", matrix[arow][checkCol])
 				
+				# keep moving the cell until we hit the end of find a non-empty cell
 				while checkCol >= 0 and checkCol < game_size and matrix[arow][checkCol] == "":
+					
+					# if we move the cell, register that we moved it so we know to add a new box
 					hasMoved = True
-					#print "Empty box at (", a, ",",checkCol,") is ", matrix[a][checkCol]
-					#print "Putting value ", matrix[a][checkCol+1], "into it"
+					
+					print("Empty box at (", arow, ",",checkCol,") is ", matrix[arow][checkCol])
+					print("Putting value ", matrix[arow][checkCol+stepDir], "into it")
+										
 					matrix[arow][checkCol] = matrix[arow][checkCol+stepDir]
 					matrix[arow][checkCol+stepDir] = ""
 					checkCol -= stepDir
-					#print_matrix()
+					print_matrix()
 	
 	for arow in range(game_size):
 		for b in range(startCol+dirCol,endCol,stepDir):
@@ -61,16 +72,16 @@ def move(matrix, direction="left"):
 				matrix[arow][b+stepDir] = ""
 				moveCol = b+stepDir
 				while moveCol < game_size-stepDir and moveCol > 0:
-					matrix[a][moveCol] = matrix[a][moveCol+stepDir]
-					matrix[a][moveCol+stepDir] = ""
+					matrix[arow][moveCol] = matrix[arow][moveCol+stepDir]
+					matrix[arow][moveCol+stepDir] = ""
 					moveCol += stepDir
 	
 	if direction is "up" or direction is "down":
 		matrix = matrix.transpose()
 	
 	return hasMoved
-
-
+	
+	
 def add_block():
 	# pick a random block value
 	value = (randrange(2)+1)*2
