bd82f97e6b3f2d48e7f561a14c7fa4d7817d2f30
Mark O <mark.orsi@gmail.com>
2018-05-05 19:11:49 +0100

    Switched to an all-number matrix and got the full game functionality working.

--- 2048.py
+++ 2048.py
@@ -6,7 +6,8 @@ def print_matrix():
 	for a in range(game_size):
 		print("|", end=' ')
 		for b in range(game_size):
-			print(matrix[a][b], "\t|", end=' ')
+			value = int(matrix[a,b]) if matrix[a,b] > 0 else ''
+			print(value, "\t|", end=' ')
 		print()
 	print
 						
@@ -18,8 +19,7 @@ def move(matrix, direction="left"):
 	
 	if direction is "up" or direction is "down":
 		matrix = matrix.transpose()
-		print(matrix)	
-	
+		
 	dirCol = directionLookup[direction][0]
 	startCol = directionLookup[direction][1]
 	endCol = directionLookup[direction][2]
@@ -38,42 +38,48 @@ def move(matrix, direction="left"):
 			# constraint is that only 2 cells can be merged together, no double merging or multi-step merging
 			
 			#if the box is not empty - if it's empty we dont care about moving anything in it
-			if matrix[arow][acol] is not " ":
+			if matrix[arow,acol] > 0:
 				# in this block, we've found a number.  
 				# start looking in the previous cell, which has index of checkCol
 				checkCol = acol+dirCol
 				
-				print("Found a thing to move. It's value is '",matrix[arow][acol],"'",)
-				print("Box of interest at (", arow, ",",acol, ") is ", matrix[arow][acol])
-				print("To its", direction, "has a value of ", matrix[arow][checkCol])
+				#print("Found a thing to move. It's value is '",matrix[arow,acol],"'",)
+				#print("Box of interest at (", arow, ",",acol, ") is ", matrix[arow,acol])
+				#print("To its", direction, "has a value of ", matrix[arow,checkCol])
 				
 				# keep moving the cell until we hit the end of find a non-empty cell
-				while checkCol >= 0 and checkCol < game_size and matrix[arow][checkCol] == "":
+				while checkCol >= 0 and checkCol < game_size and matrix[arow,checkCol] == 0:
 					
 					# if we move the cell, register that we moved it so we know to add a new box
 					hasMoved = True
 					
-					print("Empty box at (", arow, ",",checkCol,") is ", matrix[arow][checkCol])
-					print("Putting value ", matrix[arow][checkCol+stepDir], "into it")
+					#print("Empty box at (", arow, ",",checkCol,") is ", matrix[arow,checkCol])
+					#print("Putting value ", matrix[arow,checkCol+stepDir], "into it")
 										
-					matrix[arow][checkCol] = matrix[arow][checkCol+stepDir]
-					matrix[arow][checkCol+stepDir] = ""
+					matrix[arow,checkCol] = matrix[arow,checkCol+stepDir]
+					matrix[arow,checkCol+stepDir] = 0
 					checkCol -= stepDir
-					print_matrix()
+					#print_matrix()
 	
 	for arow in range(game_size):
 		for b in range(startCol+dirCol,endCol,stepDir):
 			# if the cell to the right is the same, squish em together
 			# and then move all other cells one to the left
-			if matrix[arow][b] == matrix[arow][b+stepDir] and matrix[arow][b] is not "":
+			if matrix[arow,b] == matrix[arow,b+stepDir] and matrix[arow,b] > 0:
 				
+				# print("We're looking at cell",arow,b,"in this matrix and comparing it to:",arow,b+stepDir)
 				hasMoved = True
-				matrix[arow][b] *= 2
-				matrix[arow][b+stepDir] = ""
+				# print("Value of both cells is:",matrix[arow,b],"We'll be assigning a value of:",2*int(matrix[arow,b]))
+				# print(matrix)
+				
+				matrix[arow,b] = 2 * int(matrix[arow,b])
+				
+				# print("We've just set the value to:", matrix[arow,b])
+				matrix[arow,b+stepDir] = 0
 				moveCol = b+stepDir
 				while moveCol < game_size-stepDir and moveCol > 0:
-					matrix[arow][moveCol] = matrix[arow][moveCol+stepDir]
-					matrix[arow][moveCol+stepDir] = ""
+					matrix[arow,moveCol] = matrix[arow,moveCol+stepDir]
+					matrix[arow,moveCol+stepDir] = 0
 					moveCol += stepDir
 	
 	if direction is "up" or direction is "down":
@@ -83,32 +89,37 @@ def move(matrix, direction="left"):
 	
 	
 def add_block():
+	# print("We are about to add a block to this matrix:")
+	# print_matrix()
+	
 	# pick a random block value
-	value = (randrange(2)+1)*2
+	value = int((randrange(2)+1)*2)
 	
 	foundEmpty = False
 	# pick a random coordinate and see if it is empty
 	while not foundEmpty: 
 		newRow = randrange(game_size)
 		newCol = randrange(game_size)
-		if matrix[newRow][newCol] == "":
+		if matrix[newRow,newCol] == 0:
 			foundEmpty = True
-			matrix[newRow][newCol] = value
-		
+			matrix[newRow,newCol] = value
+			
+	# print(matrix)
+	
 # Define constants
 # row and col define how many rows and columns are in the matrix
 game_size = 4
 
-matrix = [["" for a in range(game_size)] for b in range(game_size)]
-matrix = asarray(matrix)
+matrix = zeros(shape=(game_size,game_size))
 
 # Format of matrix is:
-# matrix[row id][col id]
+# matrix[row id,col id]
 # where 0,0 is the top left
 
 add_block()
 
 while True:
+	print("Here is the board:")
 	print_matrix()
 	userInput = input("Enter a direction (a=left, s=down, d=right, w=up, q=quit): ")
 	if userInput is "q": break
